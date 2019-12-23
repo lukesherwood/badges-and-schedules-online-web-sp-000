4a85491f0cb7771071803b52728d81b11eb42f24
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-23 14:08:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,8 @@ def assign_rooms(speakers)
   speakers.each_with_index { |name, index| array << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
   array
 end
+
+def printer
+ puts batch_badge_creator(attendees)
+ puts assign_rooms(speakers)
+end
