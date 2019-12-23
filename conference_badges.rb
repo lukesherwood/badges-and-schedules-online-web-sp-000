5dda3b0d8a2de02c127ad0ede370a7c18792beae
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-23 13:45:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,11 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
   
-def batch_badge_creator(array)
-  
-  return badge_array
+def batch_badge_creator(attendees)
+   array = []
+   attendees.each { |name| array << badge_maker(name) }
+   array
+end
 end
 
 end
